d73d86c68a2526eda8a0ab34a400b18cb6ace413
rahul kumar <rahul.kumar@ihub-data.iiit.ac.in>
2024-12-21 20:22:52 +0530

    redux library added

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,41 +1,40 @@
-import React from 'react'
+import React from "react";
 import {
-    MDBCard,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText,
-    MDBCardImage,
-    MDBBtn,
-    MDBRow, MDBCol
-  } from 'mdb-react-ui-kit';
-  import CardDetaile from "./productcardDetaile.json"
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+  MDBRow,
+  MDBCol,
+  MDBContainer,
+} from "mdb-react-ui-kit";
+import CardDetaile from "./productcardDetaile.json";
 const ProductCard = () => {
   return (
     <div>
+      <MDBContainer>
         <MDBRow>
-          {
-            CardDetaile && CardDetaile.map((value, key)=>{
-              return(
-                <MDBCol md='4'>
-<MDBCard key={value.id}>
-      <MDBCardImage src={value.cardImg} position='top' alt='...' />
-      <MDBCardBody>
-        <MDBCardTitle>{value.cardHeading}</MDBCardTitle>
-        <MDBCardText>
-          {value.cardDetaile}
-        </MDBCardText>
-        <MDBBtn href='#'>Button</MDBBtn>
-      </MDBCardBody>
-    </MDBCard>
-</MDBCol>
-
-              )
-            })
-          }
-        
-    </MDBRow>
+          {CardDetaile &&
+            CardDetaile.map((value, key) => {
+              return (
+                <MDBCol md="4">
+                  <MDBCard key={value.id}>
+                    <MDBCardImage src={value.img} position="top" alt="..." />
+                    <MDBCardBody>
+                      <MDBCardTitle>{value.title}</MDBCardTitle>
+                      <MDBCardText>{value.price}</MDBCardText>
+                      <MDBBtn href="#">Button</MDBBtn>
+                    </MDBCardBody>
+                  </MDBCard>
+                </MDBCol>
+              );
+            })}
+        </MDBRow>
+      </MDBContainer>
     </div>
-  )
-}
+  );
+};
 
 export default ProductCard;
